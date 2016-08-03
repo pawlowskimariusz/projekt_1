aadc1fe9c73f432957bf6a9b03e8be196b7fe276
pawlowskimariusz <mariuszpawl@live.com>
2016-08-03 07:56:25 +0200

    zmiana 1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	int a;
 	printf("Hellow world!!!!\n\r");
+	if (a==2)
+	{
+		printf("TEST");
+	}
+	else
+	{
+		printf("Test 2");
+	}
 	return 0;
 }
